228c8295e3eac35e3219632a75e79f6805444533
ltflores@gmail.com <ltflores@gmail.com@68400615-7ced-9e31-6b0b-2a6e44321c28>
2011-05-24 21:19:18 +0000

    small patch to ignore folders not conforming to our directory format

--- twitterscraper/src/tools/TweetCounts.java
+++ twitterscraper/src/tools/TweetCounts.java
@@ -15,7 +15,7 @@ public class TweetCounts {
 		TweetCounts tCnts = new TweetCounts(args);
 		tCnts.run();
 	}
-	
+
 	public TweetCounts(String args[]) {
 		if (args.length>0)
 			dataDir=args[0];
@@ -30,28 +30,41 @@ public class TweetCounts {
 		File files[]; 
 
 		files = path.listFiles(svnFilter);
-		
+
 		System.out.println("folder,party,numUsers,tweets");
 		for (File file : files){
+			if (isTweetFolder(file)){
 				String folderName = file.toString().substring((path.toString()+File.separator).length());
-				
+
 				File[] parties = file.listFiles(svnFilter);
 				for (File party : parties){
 					String partyName = party.toString().substring((file.toString()+File.separator).length());
 					File[] users = party.listFiles(svnFilter);
 					int numTweets = 0;
-					
+
 					for (File user : users){
 						numTweets += count(user);
 					}
-					
+
 					System.out.println(folderName+","+partyName+","+users.length+","+numTweets);
-					
+
 				}
+			}
+
 		}
 
 	}
-	
+
+	private boolean isTweetFolder(File folder){
+		SvnFilter svnFilter = new SvnFilter();
+		File[] subFolders = folder.listFiles(svnFilter);
+		if (subFolders.length==2){
+			if (subFolders[0].isDirectory()&&subFolders[1].isDirectory())
+				return true;
+		}
+		return false;
+	}
+
 	/**
 	 * optimized line count algorithm taken from somewhere on the vast internets...
 	 * @param filename
@@ -59,23 +72,23 @@ public class TweetCounts {
 	 * @throws IOException
 	 */
 	public int count(File filename) throws IOException {
-	    InputStream is = new BufferedInputStream(new FileInputStream(filename));
-	    try {
-	        byte[] c = new byte[1024];
-	        int count = 0;
-	        int readChars = 0;
-	        while ((readChars = is.read(c)) != -1) {
-	            for (int i = 0; i < readChars; ++i) {
-	                if (c[i] == '\n')
-	                    ++count;
-	            }
-	        }
-	        return count;
-	    } finally {
-	        is.close();
-	    }
+		InputStream is = new BufferedInputStream(new FileInputStream(filename));
+		try {
+			byte[] c = new byte[1024];
+			int count = 0;
+			int readChars = 0;
+			while ((readChars = is.read(c)) != -1) {
+				for (int i = 0; i < readChars; ++i) {
+					if (c[i] == '\n')
+						++count;
+				}
+			}
+			return count;
+		} finally {
+			is.close();
+		}
 	}
-	
+
 	/**
 	 * Naive count implementation, for the sake of comparison
 	 * @param filename
@@ -83,13 +96,13 @@ public class TweetCounts {
 	 * @throws IOException
 	 */
 	public int count2(File filename) throws IOException {
-	    BufferedReader is = new BufferedReader(new FileReader(filename));
-	    int count=0;
-	    while (is.readLine()!=null){
-	    	count++;
-	    }
-	    is.close();
-	    return count;
+		BufferedReader is = new BufferedReader(new FileReader(filename));
+		int count=0;
+		while (is.readLine()!=null){
+			count++;
+		}
+		is.close();
+		return count;
 	}
 
 
